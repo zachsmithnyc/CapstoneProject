eb44cc815b4ab5782efe7888c86dfffb8bb9609f
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2023-03-06 12:40:31 -0500

    first commit

---
+++ inventr/__init__.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+
+'''
+The application factory creates and configures the app
+'''
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE=os.path.join(app.instance_path, 'inventr.sqlite')
+    )
+
+    if test_config is None:
+        # loads the instance config for dev
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # loads the test config
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+
+    return app
