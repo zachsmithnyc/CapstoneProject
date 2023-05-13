c7d7be69259f2333e8d3053c19b38aded31d1fbb
zachsmithnyc <zachsmithnyc@gmail.com>
2023-05-13 15:22:40 -0400

    in progress

--- inventr/dashboard.py
+++ inventr/dashboard.py
@@ -16,3 +16,33 @@ def dashboard():
         'FROM inventory'
     ).fetchall()
     return render_template('dash/dash.html', inventory=inventory)
+
+@bp.route('/add_new', methods=('GET', 'POST'))
+@login_required
+def add_new():
+    if request.method == 'POST':
+        name = request.form['name']
+        desc = request.form['description']
+        price = request.form['price']
+        qis = request.form['quantity in stock']
+        reorderlevel = request.form['reorder level']
+        rtd = request.form['reorder time']
+        qir = request.form['quantity in reorder']
+        error = None
+        
+        if not name:
+            error = 'Name is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO inventory (name, desc, price, qis, reorderlevel, rtd, qir)'
+                'VALUES (?, ?, ?, ?, ?, ?, ?)',
+                (name, desc, price, qis, reorderlevel, rtd, qir)
+            )
+            db.commit()
+            return redirect(url_for('dashboard'))
+        
+    return render_template('dash/add_new.html')
