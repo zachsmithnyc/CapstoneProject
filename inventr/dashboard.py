cf9f0d70a9de7674c9838de3e1c0ff400f7a9081
zachsmithnyc <zachsmithnyc@gmail.com>
2023-05-13 14:10:01 -0400

    in progress

--- inventr/dashboard.py
+++ inventr/dashboard.py
@@ -7,3 +7,13 @@ from inventr.auth import login_required
 from inventr.db import get_db
 
 bp = Blueprint('dashboard', __name__)
+
+@bp.route('/')
+def dashboard():
+    db = get_db()
+    data = db.execute(
+        'Select *'
+        'FROM inventory'
+        'ORDER BY id ASC'
+    ).fetchall()
+    return render_template('dash/dash.html', data=data)
